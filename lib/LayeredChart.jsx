4cd4883f201ed1a4cba24918d22e465f547a742a
Sean Kelley <seansfkelley@gmail.com>
2015-12-16 18:20:12 -0800

    We're using ES6 class syntax. Figure out how to get Babel to support decorators.

--- lib/LayeredChart.jsx
+++ lib/LayeredChart.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+// import React from 'react';
+import PureRender from 'pure-render-decorator';
 
+@PureRender
 class LayeredChart extends React.Component {
   static propTypes = {
 
