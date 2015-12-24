14180ab45868ff5e8be30ad3e07499d1f605891e
Sean Kelley <seansfkelley@gmail.com>
2015-12-24 00:41:50 -0500

    Allow customization of getBoundsForInstantaeousData, though I don't use it yet.

--- src/core/findDataBounds.js
+++ src/core/findDataBounds.js
@@ -14,21 +14,22 @@ function adjustBounds(firstIndex, lastIndex, dataLength) {
   }
 }
 
-// Assumption: data is sorted by timestamp acending.
-export function getBoundsForInstantaeousData(timestampedData, timeRange) {
-  const firstIndex = _.sortedIndex(timestampedData, { timestamp: timeRange.min }, 'timestamp');
-  const lastIndex = _.sortedLastIndex(timestampedData, { timestamp: timeRange.max }, 'timestamp');
+// Assumption: data is sorted by `timestampPath` acending.
+export function getBoundsForInstantaeousData(timestampedData, timeRange, timestampPath = 'timestamp') {
+  const lowerBound = _.set({}, timestampPath, timeRange.min);
+  const upperBound = _.set({}, timestampPath, timeRange.max);
+
+  const firstIndex = _.sortedIndex(timestampedData, lowerBound, timestampPath);
+  const lastIndex = _.sortedLastIndex(timestampedData, upperBound, timestampPath);
 
   return adjustBounds(firstIndex, lastIndex, timestampedData.length);
 }
 
-// Assumption: data is sorted by timeSpan.min ascending.
+// Assumption: data is sorted by `minPath` ascending.
 export function getBoundsForTimeSpanData(timeSpanData, timeRange, minPath = 'timeSpan.min', maxPath = 'timeSpan.max') {
   // Note that these are purposely reversed. Think about it.
-  const lowerBound = {};
-  _.set(lowerBound, maxPath, timeRange.min);
-  const upperBound = {};
-  _.set(upperBound, minPath, timeRange.max);
+  const lowerBound = _.set({}, maxPath, timeRange.min);
+  const upperBound = _.set({}, minPath, timeRange.max);
 
   // Also note that this is a loose bound -- there could be spans that start later and end earlier such that
   // they don't actually fit inside the bounds, but 80-20 this is speedy and still correct, though wasteful.
