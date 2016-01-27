a547174e6ea22bf4f56cc939b38d9d263cb4080d
Sean Kelley <seansfkelley@gmail.com>
2016-01-27 01:52:19 -0800

    Export PixelRatioContext mixin for reuse.

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -1,3 +1,4 @@
 export { default as CanvasRender } from './CanvasRender';
 export { default as AnimateProps } from './AnimateProps';
+export { default as PixelRatioContext } from './PixelRatioContext';
 export { default as mixinToDecorator } from './mixinToDecorator';
