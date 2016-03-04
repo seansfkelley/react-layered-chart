890c6329f33ac1c70e23ffa64be0aec166692a94
Sean Kelley <seansfkelley@gmail.com>
2016-03-03 20:57:54 -0800

    Don't package a UMD anymore. Remove lib and dist; change organization of published artifact.

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,23 @@
 module.exports = {
-    entry: {
-        index: './src/index.js',
-        'dev-index': './src-dev/dev/dev-index.jsx'
-    },
-    output: {
-        path: './dist',
-        publicPath: '/',
-        filename: '[name].js',
-        sourceMapFilename: '[file].map',
-        library: 'LayeredChart',
-        libraryTarget: 'umd'
-    },
-    externals: {
-        react: {
-            root: 'React',
-            commonjs2: 'react',
-            commonjs: 'react',
-            amd: 'react'
-        },
-        'react-dom': {
-            root: 'ReactDOM',
-            commonjs2: 'react-dom',
-            commonjs: 'react-dom',
-            amd: 'react-dom'
-        }
-    },
-    module: {
-        loaders: [{
-            test: /\.jsx?$/,
-            exclude: /(node_modules)/,
-            loader: 'babel-loader'
-        }]
-    },
-    resolve: {
-        extensions: ['', '.js', '.jsx']
-    },
-    devtool: 'source-map'
+  entry: {
+    index: './src/index.js',
+    'dev-index': './src-dev/dev/dev-index.jsx'
+  },
+  output: {
+    path: './build-dev',
+    publicPath: '/',
+    filename: '[name].js',
+    sourceMapFilename: '[file].map'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules)/,
+      loader: 'babel-loader'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devtool: 'source-map'
 };
