b5fce1de8d3449bc1ab250f8c0f9e8040507f53c
Sean Kelley <seansfkelley@gmail.com>
2016-05-26 21:29:20 -0700

    Actually show a chart in the index.

--- dev/index.tsx
+++ dev/index.tsx
@@ -1,6 +1,32 @@
+import * as _ from 'lodash';
+import * as moment from 'moment';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+import TEST_DATA from './test-data';
+import {
+  Stack,
+  SimpleLineLayer
+} from '../src';
+
+const PARSED_TEST_DATA = TEST_DATA.map(({ timestamp, value }) => ({
+  timestamp: moment(timestamp).valueOf(),
+  value
+}));
+
+const TIMESTAMPS = _.map<{}, number>(PARSED_TEST_DATA, 'timestamp');
+const VALUES = _.map<{}, number>(PARSED_TEST_DATA, 'value');
+
 const APP_ELEMENT = document.getElementById('app');
 
-ReactDOM.render(<div className='butts'/>, APP_ELEMENT);
+const TEST_COMPONENT = (
+  <Stack>
+    <SimpleLineLayer
+      data={PARSED_TEST_DATA}
+      xDomain={{ min: _.min(TIMESTAMPS), max: _.max(TIMESTAMPS) }}
+      yDomain={{ min: _.min(VALUES), max: _.max(VALUES) }}
+    />
+  </Stack>
+);
+
+ReactDOM.render(TEST_COMPONENT, APP_ELEMENT);
