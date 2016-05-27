09704071fcd76c1e330b06a4f2aa9353a7c9a2d4
Sean Kelley <seansfkelley@gmail.com>
2016-05-26 21:07:10 -0700

    Stub out index.tsx with something.

---
+++ dev/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const APP_ELEMENT = document.getElementById('app');
+
+ReactDOM.render(<div className='butts'/>, APP_ELEMENT);
