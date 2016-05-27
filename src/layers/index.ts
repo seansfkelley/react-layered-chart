daeeef646ee749d2df5dd5baa573880131b30989
Sean Kelley <seansfkelley@gmail.com>
2016-05-26 20:05:15 -0700

    Export all the Props from all the layers.

--- src/layers/index.ts
+++ src/layers/index.ts
@@ -1,11 +1,54 @@
-export { default as AutoresizingCanvasLayer } from './AutoresizingCanvasLayer';
-export { default as BarLayer } from './BarLayer';
-export { default as BrushLayer } from './BrushLayer';
-export { default as BucketedLineLayer } from './BucketedLineLayer';
-export { default as HoverLayer } from './HoverLayer';
-export { default as InteractionCaptureLayer } from './InteractionCaptureLayer';
-export { default as PointLayer } from './PointLayer';
-export { default as SimpleLineLayer } from './SimpleLineLayer';
-export { default as TimeSpanLayer } from './TimeSpanLayer';
-export { default as XAxisLayer } from './XAxisLayer';
-export { default as YAxisLayer } from './YAxisLayer';
+export {
+  default as AutoresizingCanvasLayer,
+  Props as AutoresizingCanvasLayerProps
+} from './AutoresizingCanvasLayer';
+
+export {
+  default as BarLayer,
+  Props as BarLayerProps
+} from './BarLayer';
+
+export {
+  default as BrushLayer,
+  Props as BrushLayerProps
+} from './BrushLayer';
+
+export {
+  default as BucketedLineLayer,
+  Props as BucketedLineLayerProps
+} from './BucketedLineLayer';
+
+export {
+  default as HoverLayer,
+  Props as HoverLayerProps
+} from './HoverLayer';
+
+export {
+  default as InteractionCaptureLayer,
+  Props as InteractionCaptureLayerProps
+} from './InteractionCaptureLayer';
+
+export {
+  default as PointLayer,
+  Props as PointLayerProps
+} from './PointLayer';
+
+export {
+  default as SimpleLineLayer,
+  Props as SimpleLineLayerProps
+} from './SimpleLineLayer';
+
+export {
+  default as TimeSpanLayer,
+  Props as TimeSpanLayerProps
+} from './TimeSpanLayer';
+
+export {
+  default as XAxisLayer,
+  Props as XAxisLayerProps
+} from './XAxisLayer';
+
+export {
+  default as YAxisLayer,
+  Props as YAxisLayerProps
+} from './YAxisLayer';
