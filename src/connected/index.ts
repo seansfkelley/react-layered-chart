a8854f2bed47e3aeedf0447d60800b68ac867e39
Sean Kelley <seansfkelley@gmail.com>
2016-05-27 22:26:56 -0700

    Remove redundant import.

--- src/connected/index.ts
+++ src/connected/index.ts
@@ -3,7 +3,6 @@ export * from './layers';
 export * from './export-only/exportableActions';
 export * from './export-only/exportableSelectors';
 export * from './export-only/exportableState';
-export * from './interfaces';
 export * from './rangeUtils';
 export * from './model/constants';
 export { default as ChartProvider, Props as ChartProviderProps } from './ChartProvider';
