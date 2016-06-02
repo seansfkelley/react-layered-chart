7c5cda7d52c22b5343e0fb9fc1df4e155475a97e
Sean Kelley <seansfkelley@gmail.com>
2016-06-01 19:12:15 -0700

    Add an example of a controlled chart.

---
+++ examples/ControlledInteractiveChart.tsx
@@ -0,0 +1,71 @@
+import * as React from 'react';
+import * as moment from 'moment';
+
+import { DATA, X_DOMAIN, Y_DOMAIN } from './test-data';
+import {
+  Range,
+  ChartProvider,
+  Stack,
+  ConnectedSimpleLineLayer,
+  ConnectedInteractionCaptureLayer,
+  createStaticDataLoader,
+  enforceRangeBounds
+} from '../src';
+
+const ONE_WEEK_MS = 1000 * 60 * 60 * 24 * 7;
+
+const CHART_BOUNDS = {
+  min: X_DOMAIN.min - ONE_WEEK_MS,
+  max: X_DOMAIN.max + ONE_WEEK_MS
+};
+
+const TEST_SERIES_ID = 'foo';
+
+const DATA_LOADER = createStaticDataLoader({
+  [TEST_SERIES_ID]: DATA
+}, {
+  [TEST_SERIES_ID]: Y_DOMAIN
+});
+
+// For simplicity in this example, the controlled domain is kept on component
+// state, but you should use whatever means is appropriate for your environment.
+interface State {
+  xDomain: Range;
+}
+
+class ControlledInteractiveChart extends React.Component<{}, State> {
+  // Declare some sane default.
+  state: State = {
+    xDomain: X_DOMAIN
+  };
+
+  render() {
+    return (
+      <ChartProvider
+        seriesIds={[ TEST_SERIES_ID ]}
+        loadData={DATA_LOADER}
+        className='example-chart'
+        // Control the prop!
+        xDomain={this.state.xDomain}
+        // Capture any requested changes, such as from a ConnectedInteractionLayer.
+        onXDomainChange={this._onXDomainChange.bind(this)}
+      >
+        <Stack>
+          <ConnectedSimpleLineLayer seriesId={TEST_SERIES_ID}/>
+          <ConnectedInteractionCaptureLayer enablePan={true}/>
+        </Stack>
+      </ChartProvider>
+    );
+  }
+
+  private _onXDomainChange(xDomain: Range) {
+    // Use the provided utility to munge the X domain to something acceptable.
+    this.setState({
+      xDomain: enforceRangeBounds(xDomain, CHART_BOUNDS)
+    });
+  };
+}
+
+const CHART = <ControlledInteractiveChart/>;
+
+export default CHART;
--- examples/index.tsx
+++ examples/index.tsx
@@ -3,6 +3,7 @@ import * as ReactDOM from 'react-dom';
 
 import STATIC_CHART from './StaticChart';
 import BASIC_INTERACTIVE_CHART from './BasicInteractiveChart';
+import CONTROLLED_INTERACTIVE_CHART from './ControlledInteractiveChart';
 
 import '../styles/index.less';
 import './example-styles.less';
@@ -15,6 +16,8 @@ const TEST_COMPONENT = (
     {STATIC_CHART}
     <div className='explanation'>This is a more complex chart. Drag to pan and scroll to zoom.</div>
     {BASIC_INTERACTIVE_CHART}
+    <div className='explanation'>This chart is pannable, but with limits.</div>
+    {CONTROLLED_INTERACTIVE_CHART}
   </div>
 );
 
