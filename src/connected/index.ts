b5f326f438a3ec6b0beb59bad57477c84c904c83
Sean Kelley <skelley@palantir.com>
2016-10-11 19:09:02 -0700

    Add missing export of chainLoaders.

--- src/connected/index.ts
+++ src/connected/index.ts
@@ -5,5 +5,6 @@ export * from './export-only/exportableActions';
 export * from './export-only/exportableSelectors';
 export * from './export-only/exportableState';
 export * from './dataUtils';
+export * from './loaderUtils';
 export * from './model/constants';
 export { default as ChartProvider, Props as ChartProviderProps } from './ChartProvider';
