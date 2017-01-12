4f37b040d737eafbea6c5500999b5d943d595939
Suchan Lee <suchanl@palantir.com>
2017-01-11 21:04:00 -0800

    use @types/d3-scale for typings

--- extra-typings.d.ts
+++
@@ -1,6 +0,0 @@
-// I don't depend on the d3 typings because as of this writing they don't have the proper typings for all
-// of these libraries and I'm not going to go rewrite them all now.
-declare module 'd3-scale' {
-  var scale: any;
-  export = scale;
-}
